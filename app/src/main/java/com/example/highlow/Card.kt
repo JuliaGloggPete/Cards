9c771f76069863daf92c9249539f5429ee41fef8
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2022-11-16 13:16:57 +0100

    very little code to see if pushing and pulling works now

--- app/src/main/java/com/example/highlow/Card.kt
+++ app/src/main/java/com/example/highlow/Card.kt
@@ -2,7 +2,7 @@ package com.example.highlow
 
 import android.widget.ImageView
 
-class Card (var value: Int, var suits: String, var cardImage: ImageView) {
+class Card (var value: Int, var suits: String, ) {
 
 
 }
--- app/src/main/java/com/example/highlow/Deck.kt
+++ app/src/main/java/com/example/highlow/Deck.kt
@@ -1,9 +1,17 @@
 package com.example.highlow
 
+import android.util.Log
+import android.widget.ImageView
+
 class Deck {
 
+    val cardList = mutableListOf<Card>()
+
+    fun createCards(){
+        // var AceOfSpades :ImageView
 
 
-
+                cardList.add(Card(1,"Spades"))
+            }}
 
 }
